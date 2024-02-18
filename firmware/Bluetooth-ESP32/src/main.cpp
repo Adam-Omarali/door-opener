0d84a42a93a2a9e1aae891c2ead9d487c03594f8
Adam <adamomarali37@gmail.com>
2024-02-18 06:47:07 -0500

    esp32 ble

---
+++ firmware/Bluetooth-ESP32/src/main.cpp
@@ -0,0 +1,31 @@
+//This example code is in the Public Domain (or CC0 licensed, at your option.)
+//By Evandro Copercini - 2018
+//
+//This example creates a bridge between Serial and Classical Bluetooth (SPP)
+//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(19200);
+  SerialBT.begin("ESP32-BLE"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+}
+
+void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    char c = SerialBT.read();
+    Serial.write(c);
+    Serial.println(c);
+  }
+  delay(20);
+}
