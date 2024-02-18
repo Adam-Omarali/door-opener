a6f74ccb02db8f34edab5ebff5412002a273a0e6
Joseph Mi <jmjosephmi@gmail.com>
2024-02-17 23:37:09 -0500

    gyrocope

--- firmware/motor-control/src/main.cpp
+++ firmware/motor-control/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Stepper.h> 
+#include "gyro.cpp"
 
 #define INTERBOARD_ANALOG A0
 #define TOLERANCE 23
@@ -16,6 +17,7 @@ Stepper stepper(STEPS, 4, 5, 6, 7);
 
 void setup()
 {
+  angle_setup();
   Serial.begin(9600);
   // set the speed of the motor to 30 RPMs
   stepper.setSpeed(80);
@@ -29,6 +31,7 @@ void setup()
 
 void loop()
 {
+  get_angle();
   Serial.println("Forward");
   stepper.step(STEPS * 10);
 
@@ -42,43 +45,3 @@ void loop()
     digitalWrite(TEST_LED, LOW); 
   }
 }
-// #define STEPS 200
-// // Define stepper motor connections and motor interface type. Motor interface type must be set to 1 when using a driver
-
-// #define motorInterfaceType 1
-// Stepper stepper(STEPS, 2, 3); // Pin 2 connected to DIRECTION & Pin 3 connected to STEP Pin of Driver
-
-// int Pval = 0;
-
-// int potVal = 0;
-
-
-// void setup() {
-//   // Set the maximum speed in steps per second:
-//   Serial.begin(9600);
-
-//   stepper.setSpeed(1000);
-// }
-
-// void loop() {
-//   stepper.step(10);
-  // potVal = map(analogRead(A0),0,1024,0,500);
-  // if (potVal>Pval){
-  //   stepper.step(10);
-  //   Serial.println("forward");
-  // }
-  
-  //Serial.println(potVal);
-
-  //if (potVal<Pval){
-  
-  //Serial.println(potVal);
-
-  //if (potVal<Pval){
-  //  stepper.step(10);
-    //Serial.print("backward");
-  //}
-
-
-// Pval = potVal;
-// }
